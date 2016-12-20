87eb2db6adab2681e66c0da4fc706e27da8d4d2b
Jade <jade0@hotmail.fr>
2016-12-20 14:31:26 +0100

    ajout photos

--- Site web/Real-Estate/js/freelancer.js
+++ Site web/Real-Estate/js/freelancer.js
@@ -7,7 +7,7 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 62)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -23,8 +23,8 @@
     $(".eq-height").height(maxHeight);
 	});
 
-
-
+	
+	
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
--- Site web/Real-Estate/mail/contact_me.php
+++ Site web/Real-Estate/mail/contact_me.php
@@ -1,26 +1,37 @@
 <?php
+if(empty($_SERVER['CONTENT_TYPE']))
+{ 
+  $_SERVER['CONTENT_TYPE'] = "application/x-www-form-urlencoded"; 
+}
+
+// Update on 2016-11-02: changed $_POST by $_REQUEST everywhere
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+var_dump($_REQUEST);
+if(empty($_REQUEST['name'])  		||
+   empty($_REQUEST['email']) 		||
+   empty($_REQUEST['message'])
+)   
+// filter_var is not available in this php version
+   //||
+   //!filter_var($_REQUEST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "No arguments or invalid arguments provided!";
 	return false;
    }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$name = strip_tags(htmlspecialchars($_REQUEST['name']));
+$email_address = strip_tags(htmlspecialchars($_REQUEST['email']));
+$phone = strip_tags(htmlspecialchars($_REQUEST['phone']));
+$message = strip_tags(htmlspecialchars($_REQUEST['message']));
 	
 // Create the email and send the message
-$to = 'jade0@hotmail.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'mh@realestatecaretaking.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@realestate.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
+ 
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return false;			
 ?>
